255e3bd5120ee839ee6392c3ed0ba7c1e8811bfc
Sandy Smith <toosandra@gmail.com>
2020-09-11 21:54:05 -0400

    Added 3rd party api coding to utils>appjs.

---
+++ client/src/utils/API.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export default  {
+  getRegion: (region) => 
+  axios ({ 
+    "method":"GET",
+    "url":"https://coronavirus-map.p.rapidapi.com/v1/summary/region",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"coronavirus-map.p.rapidapi.com",
+    "x-rapidapi-key":"0aa3f25180msh76e118582c50a44p19178djsna4b1193c345a",
+    "useQueryString":true
+    },"params":{
+    "region":region
+    
+    }
+    })
+  }
