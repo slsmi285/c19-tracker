56cdbd861d8abc0e221df39f89667455f950db59
Sandy Smith <toosandra@gmail.com>
2020-09-11 23:35:07 -0400

    completed Hero and css file

---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+function Hero(props) {
+  return (
+    <div className="hero text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
+      {props.children}
+    </div>
+  );
+}
+export default Hero;
