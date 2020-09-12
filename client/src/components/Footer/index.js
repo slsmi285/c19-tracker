9ecd1e2b00579a31e6134d29a37bf7df68939757
Sandy Smith <toosandra@gmail.com>
2020-09-11 23:33:56 -0400

    completed footer and css file

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Stay Safe!</span>
+    </footer>
+  );
+}
+export default Footer;
