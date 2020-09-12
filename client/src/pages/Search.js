1c285e076935b5377eb46ebe30f3ef2b0601f223
Sandy Smith <toosandra@gmail.com>
2020-09-11 22:24:51 -0400

    .

---
+++ client/src/pages/Search.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import API from "../components/utils/API";
+import Container from "../components/Container";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
+import Alert from "../components/Alert";
