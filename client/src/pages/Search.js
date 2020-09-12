276dc21dfad124049af3c5f88cae077a9ab4c8dc
Sandy Smith <toosandra@gmail.com>
2020-09-11 22:27:41 -0400

    Finish coding html rendering for the api search and handleform.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,3 +4,32 @@ import Container from "../components/Container";
 import SearchForm from "../components/SearchForm";
 import SearchResults from "../components/SearchResults";
 import Alert from "../components/Alert";
+
+class Search extends Component {
+    state = {
+      region: "",
+      active_cases: "",
+      error: ""
+    };
+
+     // When the component mounts, get a result of active cases
+  componentDidMount() {
+    API.getRegion()
+      .then(res => this.setState({ region: res.data.data.summary.active_cases }))
+      .catch(err => console.log(err));
+  }
+  handleInputChange = event => {
+    this.setState({ region: event.target.value });
+  };
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.getRegion(this.state.region)
+      .then(res => {
+        if (res.data.status === "error") {
+          throw new Error(res.data.message);
+        }
+        this.setState({ active_cases: res.data.data.summary.active_cases, error: "" });
+        console.log(res.data.data.summary.active_cases)
+      })
+      .catch(err => this.setState({ error: err.message }));
+  };
